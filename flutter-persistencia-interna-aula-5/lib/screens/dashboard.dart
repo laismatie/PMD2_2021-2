5614626bd58dd2cbea5f7f79d024be9835fa7f0a
Laís Matie <lais@jeraapps.com>
2021-08-31 16:36:11 -0400

    adicionando scroll nas ações

--- flutter-persistencia-interna-aula-5/lib/screens/dashboard.dart
+++ flutter-persistencia-interna-aula-5/lib/screens/dashboard.dart
@@ -17,45 +17,82 @@ class Dashboard extends StatelessWidget {
             padding: const EdgeInsets.all(8.0),
             child: Image.asset('images/bytebank_logo.png'),
           ),
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Material(
-              color: Theme.of(context).primaryColor,
-              child: InkWell(
-                onTap: () {
-                  Navigator.of(context).push(
-                    MaterialPageRoute(
-                      builder: (context) => ContactsList(),
-                    ),
-                  );
+          Container(
+            height: 120,
+            child: ListView(scrollDirection: Axis.horizontal, children: <Widget>[
+              _FeatureItem(
+                'Transfer',
+                Icons.monetization_on,
+                onClick: () {
+                  _showContactsList(context);
                 },
-                child: Container(
-                  padding: EdgeInsets.all(8.0),
-                  height: 100,
-                  width: 150,
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: <Widget>[
-                      Icon(
-                        Icons.monetization_on,
-                        color: Colors.white,
-                        size: 24.0,
-                      ),
-                      Text(
-                        'Transfer',
-                        style: TextStyle(
-                          color: Colors.white,
-                          fontSize: 16.0,
-                        ),
-                      ),
-                    ],
-                  ),
-                ),
               ),
-            ),
-          )
+              _FeatureItem(
+                'Transaction Feed',
+                Icons.description,
+                onClick: () => print('Transaction feed was clicked'),
+              ),
+            ]),
+          ),
         ],
+      ),
+    );
+  }
+}
+
+void _showContactsList(BuildContext context) {
+  Navigator.of(context).push(
+    MaterialPageRoute(
+      builder: (context) => ContactsList(),
+    ),
+  );
+}
+
+class _FeatureItem extends StatelessWidget {
+  final String name;
+  final IconData icon;
+  final Function onClick;
+
+  _FeatureItem(this.name, this.icon, {@required this.onClick});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Material(
+        color: Theme.of(context).primaryColor,
+        child: InkWell(
+          onTap: () {
+            onClick();
+            Navigator.of(context).push(
+              MaterialPageRoute(
+                builder: (context) => ContactsList(),
+              ),
+            );
+          },
+          child: Container(
+            padding: EdgeInsets.all(8.0),
+            width: 150,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                Icon(
+                  icon,
+                  color: Colors.white,
+                  size: 24.0,
+                ),
+                Text(
+                  name,
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 16.0,
+                  ),
+                )
+              ],
+            ),
+          ),
+        ),
       ),
     );
   }
